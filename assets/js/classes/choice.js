ee64afc7cae2da5da6593359146edf9968d399e1
LeLwy <louis.chancioux@gmail.com>
2023-01-16 09:58:16 +0000

    Init des classes

---
+++ assets/js/classes/choice.js
@@ -0,0 +1,6 @@
+class Choice{
+    
+    #room;
+    #situation;
+    #
+}
---
+++ assets/js/classes/game.js
@@ -0,0 +1,35 @@
+class Game{
+    
+    #character;
+    #choices;
+    
+    constructor(){
+        
+        this.#character = character;
+        this.#choices = [];
+    }
+    
+    get choices (){
+        return this.#choices;
+    }
+
+    set choices (choices){
+        this.#choices = choices;
+    }
+    
+    get character (){
+        return this.#character;
+    }
+
+    set character (character){
+        this.#character = character;
+    }
+    
+    redirect(choice){
+        
+    }
+    
+    save(){
+        
+    }
+}
